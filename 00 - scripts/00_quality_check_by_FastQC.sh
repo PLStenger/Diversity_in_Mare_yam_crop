826e802f5801dde66f4e836f4020fe42c595df58
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2021-03-29 10:54:46 +1100

    Update and rename 00 - quality check by FastQC.sh to 00_quality_check_by_FastQC.sh

---
+++ 00 - scripts/00_quality_check_by_FastQC.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# installing FastQC from https://www.bioinformatics.babraham.ac.uk/projects/download.html
+# FastQC v0.11.9 (Mac DMG image)
+
+# Correct tool citation : Andrews, S. (2010). FastQC: a quality control tool for high throughput sequence data.
+
+WORKING_DIRECTORY = /Users/pierre-louisstenger/Documents/PostDoc\ 02\ MetaBarcoding\ IAC/02\ -\ Data/05\ -\ Mare\ ignames/Diversity_in_Mare_yam_crop/00\ -\ scripts
