d3aca4d6352af4c11e0e1941f5d5dc57f6873109
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2021-03-29 14:54:59 +1100

    Update 03_trimmomatic_q30.sh

--- 00_scripts/03_trimmomatic_q30.sh
+++ 00_scripts/03_trimmomatic_q30.sh
@@ -47,11 +47,11 @@ trimmomatic PE -Xmx60G \
         $DATAOUTPUT_ITS2_fungi/"${LINE}"_R1.single.fastq.gz \
         $DATAOUTPUT_ITS2_fungi/"${LINE}"_R2.paired.fastq.gz \
         $DATAOUTPUT_ITS2_fungi/"${LINE}"_R2.single.fastq.gz \
-        ILLUMINACLIP:"$ADAPTERFILE":2:30:10 \ #Cut adapter and other illumina-specific sequences from the read.
-        LEADING:30 \ # Cut FILEs off the start of a read, if below a threshold quality
-        TRAILING:30 \ # Cut FILEs off the end of a read, if below a threshold quality
-        SLIDINGWINDOW:26:30 \ # Performs a sliding window trimming approach. It starts scanning at the 5‟ end and clips the read once the average quality within the window falls below a threshold.
-        MINLEN:150  # Drop the read if it is below a specified length
+        ILLUMINACLIP:"$ADAPTERFILE":2:30:10 \
+        LEADING:30 \
+        TRAILING:30 \
+        SLIDINGWINDOW:26:30 \
+        MINLEN:150
         
 done < list_04.txt
 
