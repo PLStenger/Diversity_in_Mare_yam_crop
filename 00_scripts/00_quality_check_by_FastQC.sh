0038c32691f552f386753cd698eb8c68e7ca4351
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2021-03-29 11:25:30 +1100

    Update 00_quality_check_by_FastQC.sh

--- 00_scripts/00_quality_check_by_FastQC.sh
+++ 00_scripts/00_quality_check_by_FastQC.sh
@@ -5,4 +5,22 @@
 
 # Correct tool citation : Andrews, S. (2010). FastQC: a quality control tool for high throughput sequence data.
 
-WORKING_DIRECTORY = /Users/pierre-louisstenger/Documents/PostDoc\ 02\ MetaBarcoding\ IAC/02\ -\ Data/05\ -\ Mare\ ignames/Diversity_in_Mare_yam_crop/00\ -\ scripts
+WORKING_DIRECTORY = /Users/pierre-louisstenger/Documents/PostDoc\ 02\ MetaBarcoding\ IAC/02\ -\ Data/05\ -\ Mare\ ignames/Diversity_in_Mare_yam_crop/
+DATA_DIRECTORY_ITS2_fungi = /Users/pierre-louisstenger/Documents/PostDoc 02 MetaBarcoding IAC/02 - Data/05 - Mare ignames/Diversity_in_Mare_yam_crop/00_raw_data/ITS2_fungi
+DATA_DIRECTORY_V4_bacteria = /Users/pierre-louisstenger/Documents/PostDoc 02 MetaBarcoding IAC/02 - Data/05 - Mare ignames/Diversity_in_Mare_yam_crop/00_raw_data/V4_bacteria
+OUT_PUT_ITS2_fungi = /Users/pierre-louisstenger/Documents/PostDoc 02 MetaBarcoding IAC/02 - Data/05 - Mare ignames/Diversity_in_Mare_yam_crop/01_quality_check/OUT_PUT_ITS2_fungi
+OUT_PUT_V4_bacteria = /Users/pierre-louisstenger/Documents/PostDoc 02 MetaBarcoding IAC/02 - Data/05 - Mare ignames/Diversity_in_Mare_yam_crop/01_quality_check/OUT_PUT_V4_bacteria
+
+cd $WORKING_DIRECTORY
+
+# For ITS2_fungi
+for FILE in $(ls $DATA_DIRECTORY_ITS2_fungi/*.fastq.gz)
+do
+      fastqc $FILE -o $OUT_PUT_ITS2_fungi
+done ;
+
+# For V4_bacteria
+for FILE in $(ls $DATA_DIRECTORY_V4_bacteria/*.fastq.gz)
+do
+      fastqc $FILE -o $OUT_PUT_V4_bacteria
+done ;
