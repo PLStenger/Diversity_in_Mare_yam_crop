199b2e7877067e4c26d4410cb143b66f624fcc78
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2021-04-08 14:41:04 +1100

    Create 10_qiime2_tree.sh

---
+++ 00_scripts/10_qiime2_tree.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+DATADIRECTORY_ITS2_fungi=/Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_IAC/02_Data/05_Mare_ignames/Diversity_in_Mare_yam_crop/05_QIIME2/ITS2/
+DATADIRECTORY_V4_bacteria=/Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_IAC/02_Data/05_Mare_ignames/Diversity_in_Mare_yam_crop/05_QIIME2/V4/
+
+# Aim: construct a rooted phylogenetic tree
+
+###############################################################
+### For Fungi
+###############################################################
+
+cd $DATADIRECTORY_ITS2_fungi
+
+eval "$(conda shell.bash hook)"
+conda activate qiime2-2019.10
+
+#carry out a multiple seqeunce alignment using Mafft
+ qiime alignment mafft \
+  --i-sequences RepSeq.qza \
+  --o-alignment aligned-RepSeq.qza
+
+#mask (or filter) the alignment to remove positions that are highly variable. These positions are generally considered to add noise to a resulting phylogenetic tree.
+qiime alignment mask \
+  --i-alignment aligned-RepSeq.qza \
+  --o-masked-alignment masked-aligned-RepSeq.qza
+
+#create the tree using the Fasttree program
+qiime phylogeny fasttree \
+  --i-alignment masked-aligned-RepSeq.qza \
+  --o-tree unrooted-tree.qza
+
+#root the tree using the longest root
+qiime phylogeny midpoint-root \
+  --i-tree unrooted-tree.qza \
+  --o-rooted-tree rooted-tree.qza
+  
+  ###############################################################
+### For Bacteria
+###############################################################
+
+cd $DATADIRECTORY_V4_bacteria
+
+eval "$(conda shell.bash hook)"
+conda activate qiime2-2019.10
+
+
+#carry out a multiple seqeunce alignment using Mafft
+ qiime alignment mafft \
+  --i-sequences RepSeq.qza \
+  --o-alignment aligned-RepSeq.qza
+
+#mask (or filter) the alignment to remove positions that are highly variable. These positions are generally considered to add noise to a resulting phylogenetic tree.
+qiime alignment mask \
+  --i-alignment aligned-RepSeq.qza \
+  --o-masked-alignment masked-aligned-RepSeq.qza
+
+#create the tree using the Fasttree program
+qiime phylogeny fasttree \
+  --i-alignment masked-aligned-RepSeq.qza \
+  --o-tree unrooted-tree.qza
+
+#root the tree using the longest root
+qiime phylogeny midpoint-root \
+  --i-tree unrooted-tree.qza \
+  --o-rooted-tree rooted-tree.qza
