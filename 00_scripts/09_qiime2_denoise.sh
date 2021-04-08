862a442fdfa37ac1958631e14c3dc530b5f2f228
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2021-04-08 12:09:05 +1100

    Update 09_qiime2_denoise.sh

--- 00_scripts/09_qiime2_denoise.sh
+++ 00_scripts/09_qiime2_denoise.sh
@@ -3,6 +3,8 @@
 DATADIRECTORY_ITS2_fungi=/Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_IAC/02_Data/05_Mare_ignames/Diversity_in_Mare_yam_crop/05_QIIME2/ITS2/
 DATADIRECTORY_V4_bacteria=/Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_IAC/02_Data/05_Mare_ignames/Diversity_in_Mare_yam_crop/05_QIIME2/V4/
 
+# https://chmi-sops.github.io/mydoc_qiime2.html
+
 # https://docs.qiime2.org/2021.2/plugins/available/dada2/denoise-single/
 # Aim: denoises single-end sequences, dereplicates them, and filters
 # chimeras and singletons sequences
@@ -27,6 +29,14 @@ qiime dada2 denoise-single --i-demultiplexed-seqs demux.qza \
                            --o-representative-sequences RepSeq.qza \
                            --o-table Table.qza \
                            --o-denoising-stats Stats.qza
+                           
+qiime feature-table summarize \
+  --i-table Table.qza \
+  --o-visualization Table.qzv \
+  --m-sample-metadata-file sample-metadata.tsv
+qiime feature-table tabulate-seqs \
+  --i-data RepSeq.qza \
+  --o-visualization RepSeq.qzv                           
 
 ###############################################################
 ### For Bacteria
@@ -45,3 +55,12 @@ qiime dada2 denoise-single --i-demultiplexed-seqs demux.qza \
                            --o-representative-sequences RepSeq.qza \
                            --o-table Table.qza \
                            --o-denoising-stats Stats.qza
+                           
+                           
+qiime feature-table summarize \
+  --i-table Table.qza \
+  --o-visualization Table.qzv \
+  --m-sample-metadata-file sample-metadata.tsv
+qiime feature-table tabulate-seqs \
+  --i-data RepSeq.qza \
+  --o-visualization RepSeq.qzv    
