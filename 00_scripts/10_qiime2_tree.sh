0bc8552d0b06eb7331183ac85c1e5835faa4f32b
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2021-04-08 16:40:53 +1100

    Update 10_qiime2_tree.sh

--- 00_scripts/10_qiime2_tree.sh
+++ 00_scripts/10_qiime2_tree.sh
@@ -33,11 +33,11 @@ conda activate qiime2-2019.10
 #qiime phylogeny midpoint-root \
 #  --i-tree unrooted-tree.qza \
 #  --o-rooted-tree rooted-tree.qza
-  
+#  
 #export the tree  
-qiime tools export \
-  --input-path unrooted-tree.qza \
-  --output-path $DATADIRECTORY_ITS2_fungi 
+#qiime tools export \
+#  --input-path unrooted-tree.qza \
+#  --output-path $DATADIRECTORY_ITS2_fungi 
   
 # This out put is in Newick format, see http://scikit-bio.org/docs/latest/generated/skbio.io.format.newick.html  
 # See it on https://itol.embl.de
@@ -52,25 +52,25 @@ eval "$(conda shell.bash hook)"
 conda activate qiime2-2019.10
 
 
-##carry out a multiple seqeunce alignment using Mafft
-# qiime alignment mafft \
-#  --i-sequences RepSeq.qza \
-#  --o-alignment aligned-RepSeq.qza
-#
+#carry out a multiple seqeunce alignment using Mafft
+ qiime alignment mafft \
+  --i-sequences RepSeq.qza \
+  --o-alignment aligned-RepSeq.qza
+
 ##mask (or filter) the alignment to remove positions that are highly variable. These positions are generally considered to add noise to a resulting phylogenetic tree.
-#qiime alignment mask \
-#  --i-alignment aligned-RepSeq.qza \
-#  --o-masked-alignment masked-aligned-RepSeq.qza
-#
+qiime alignment mask \
+  --i-alignment aligned-RepSeq.qza \
+  --o-masked-alignment masked-aligned-RepSeq.qza
+
 ##create the tree using the Fasttree program
-#qiime phylogeny fasttree \
-#  --i-alignment masked-aligned-RepSeq.qza \
-#  --o-tree unrooted-tree.qza
-#
+qiime phylogeny fasttree \
+  --i-alignment masked-aligned-RepSeq.qza \
+  --o-tree unrooted-tree.qza
+
 ##root the tree using the longest root
-#qiime phylogeny midpoint-root \
-#  --i-tree unrooted-tree.qza \
-#  --o-rooted-tree rooted-tree.qza
+qiime phylogeny midpoint-root \
+  --i-tree unrooted-tree.qza \
+  --o-rooted-tree rooted-tree.qza
   
   
 #export the tree  
