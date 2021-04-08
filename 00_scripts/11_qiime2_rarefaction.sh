61c42650b16d026f358d73b70b4da127466eb3d2
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2021-04-08 16:11:44 +1100

    Update 11_qiime2_rarefaction.sh

--- 00_scripts/11_qiime2_rarefaction.sh
+++ 00_scripts/11_qiime2_rarefaction.sh
@@ -5,6 +5,9 @@ DATADIRECTORY_V4_bacteria=/Users/pierre-louisstenger/Documents/PostDoc_02_MetaBa
 
 # Aim: rarefy a feature table to compare alpha/beta diversity results
 
+# A good forum to understand what it does :
+# https://forum.qiime2.org/t/can-someone-help-in-alpha-rarefaction-plotting-depths/4580/16
+
 ###############################################################
 ### For Fungi
 ###############################################################
@@ -14,12 +17,20 @@ cd $DATADIRECTORY_ITS2_fungi
 eval "$(conda shell.bash hook)"
 conda activate qiime2-2019.10
 
+# Note: max-depth should be chosen based on table.qzv
+
 qiime diversity alpha-rarefaction \
   --i-table Table.qza \
   --i-phylogeny rooted-tree.qza \
-  --p-max-depth 4000 \
+  --p-max-depth 78 \
   --m-metadata-file sample-metadata.tsv \
   --o-visualization alpha-rarefaction.qzv
+  
+# Note: Interpreting alpha diversity metrics: it is important to understand that certain metrics are stricly qualitative (presence/absence), 
+# that is they only take diversity into account, often referred to as richness of the community (e.g. observed otus). 
+# In contrast, other methods are quantitative in that they consider both richness and abundance across samples, commonly referred to as evenness (e.g. Shannon). 
+# Yet other methods take phylogenetic distance into account by asking how diverse the phylogenetic tree is for each sample. 
+# These phylogenetic tree-based methods include the popular Faith’s PD, which calculates the sum of the branch length covered by a sample
   
 ###############################################################
 ### For Bacteria
@@ -30,11 +41,17 @@ cd $DATADIRECTORY_V4_bacteria
 eval "$(conda shell.bash hook)"
 conda activate qiime2-2019.10
 
+# Note: max-depth should be chosen based on table.qzv
+
 qiime diversity alpha-rarefaction \
   --i-table Table.qza \
   --i-phylogeny rooted-tree.qza \
-  --p-max-depth 4000 \
+  --p-max-depth 78 \
   --m-metadata-file sample-metadata.tsv \
   --o-visualization alpha-rarefaction.qzv
   
-  
+  # Note: Interpreting alpha diversity metrics: it is important to understand that certain metrics are stricly qualitative (presence/absence), 
+# that is they only take diversity into account, often referred to as richness of the community (e.g. observed otus). 
+# In contrast, other methods are quantitative in that they consider both richness and abundance across samples, commonly referred to as evenness (e.g. Shannon). 
+# Yet other methods take phylogenetic distance into account by asking how diverse the phylogenetic tree is for each sample. 
+# These phylogenetic tree-based methods include the popular Faith’s PD, which calculates the sum of the branch length covered by a sample
