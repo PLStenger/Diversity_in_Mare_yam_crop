c25e10c9b5efd54dfb684d67761c5ce174d14545
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2021-04-08 16:34:21 +1100

    Update 08_qiime2_import.sh

--- 00_scripts/08_qiime2_import.sh
+++ 00_scripts/08_qiime2_import.sh
@@ -13,19 +13,19 @@ MANIFEST_BACTERIA=/Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_
 ### For Fungi
 ###############################################################
 
-cd $DATADIRECTORY_ITS2_fungi
-
-eval "$(conda shell.bash hook)"
-conda activate qiime2-2019.10
-
-qiime tools import --type 'SampleData[SequencesWithQuality]' \
-			    --input-path  $MANIFEST_FUNGI \
-			    --output-path $OUTPUT_ITS2_fungi/demux.qza \
-			    --input-format SingleEndFastqManifestPhred33
-
-cd $OUTPUT_ITS2_fungi
-
-qiime demux summarize --i-data demux.qza --o-visualization demux.qzv
+#cd $DATADIRECTORY_ITS2_fungi
+#
+#eval "$(conda shell.bash hook)"
+#conda activate qiime2-2019.10
+#
+#qiime tools import --type 'SampleData[SequencesWithQuality]' \
+#			    --input-path  $MANIFEST_FUNGI \
+#			    --output-path $OUTPUT_ITS2_fungi/demux.qza \
+#			    --input-format SingleEndFastqManifestPhred33
+#
+#cd $OUTPUT_ITS2_fungi
+#
+#qiime demux summarize --i-data demux.qza --o-visualization demux.qzv
 
 # for vizualisation :
 # https://view.qiime2.org
