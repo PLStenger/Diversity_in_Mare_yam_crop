362edf62c284d22109e3125ce20e0634c8b263f6
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2021-04-08 11:36:28 +1100

    Update 09_qiime2_denoise.sh

--- 00_scripts/09_qiime2_denoise.sh
+++ 00_scripts/09_qiime2_denoise.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 
 DATADIRECTORY_ITS2_fungi=/Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_IAC/02_Data/05_Mare_ignames/Diversity_in_Mare_yam_crop/05_QIIME2/ITS2/
-
-cd $DATADIRECTORY_ITS2_fungi
+DATADIRECTORY_V4_bacteria=/Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_IAC/02_Data/05_Mare_ignames/Diversity_in_Mare_yam_crop/05_QIIME2/V4/
 
 # https://docs.qiime2.org/2021.2/plugins/available/dada2/denoise-single/
 # Aim: denoises single-end sequences, dereplicates them, and filters
 # chimeras and singletons sequences
 # Use: qiime dada2 denoise-single [OPTIONS]
 
+# DADA2 method
+
+###############################################################
+### For Fungi
+###############################################################
+
+cd $DATADIRECTORY_ITS2_fungi
+
 eval "$(conda shell.bash hook)"
 conda activate qiime2-2019.10
-
-# DADA2 method
 
 qiime dada2 denoise-single --i-demultiplexed-seqs demux.qza \
                            --p-n-threads 6 \
@@ -23,4 +28,20 @@ qiime dada2 denoise-single --i-demultiplexed-seqs demux.qza \
                            --o-table Table.qza \
                            --o-denoising-stats Stats.qza
 
+###############################################################
+### For Bacteria
+###############################################################
 
+cd $DATADIRECTORY_V4_bacteria
+
+eval "$(conda shell.bash hook)"
+conda activate qiime2-2019.10
+
+qiime dada2 denoise-single --i-demultiplexed-seqs demux.qza \
+                           --p-n-threads 6 \
+                           --p-chimera-method 'consensus' \
+                           --p-trunc-len 0 \
+                           --p-trunc-q 0 \
+                           --o-representative-sequences RepSeq.qza \
+                           --o-table Table.qza \
+                           --o-denoising-stats Stats.qza
