6277157c15b24e1e7f40ea4cbfb108db52c1c099
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2021-04-16 15:30:02 +1100

    Update 09_qiime2_denoise.sh

--- 00_scripts/09_qiime2_denoise.sh
+++ 00_scripts/09_qiime2_denoise.sh
@@ -16,27 +16,109 @@ DATADIRECTORY_V4_bacteria=/Users/pierre-louisstenger/Documents/PostDoc_02_MetaBa
 ### For Fungi
 ###############################################################
 
-#cd $DATADIRECTORY_ITS2_fungi
-#
-#eval "$(conda shell.bash hook)"
-#conda activate qiime2-2019.10
-#
-#qiime dada2 denoise-single --i-demultiplexed-seqs demux.qza \
-#                           --p-n-threads 6 \
-#                           --p-chimera-method 'consensus' \
-#                           --p-trunc-len 0 \
-#                           --p-trunc-q 0 \
-#                           --o-representative-sequences RepSeq.qza \
-#                           --o-table Table.qza \
-#                           --o-denoising-stats Stats.qza
-#                           
-#qiime feature-table summarize \
-#  --i-table Table.qza \
-#  --o-visualization Table.qzv \
-#  --m-sample-metadata-file sample-metadata.tsv
-#qiime feature-table tabulate-seqs \
-#  --i-data RepSeq.qza \
-#  --o-visualization RepSeq.qzv                           
+cd $DATADIRECTORY_ITS2_fungi
+
+eval "$(conda shell.bash hook)"
+conda activate qiime2-2019.10
+
+# dada2_denoise :
+#################
+
+# Aim: denoises single-end sequences, dereplicates them, and filters
+#      chimeras and singletons sequences
+#      Use: qiime dada2 denoise-single [OPTIONS]
+
+qiime dada2 denoise-single --i-demultiplexed-seqs demux.qza \
+                           --p-n-threads 6 \
+                           --p-chimera-method 'consensus' \
+                           --p-trunc-len 0 \
+                           --p-trunc-q 0 \
+                           --o-representative-sequences RepSeq.qza \
+                           --o-table Table.qza \
+                           --o-denoising-stats Stats.qza
+
+# sequence_contamination_filter :
+#################################
+
+# Aim: aligns feature sequences to a set of reference sequences
+#      to identify sequences that hit/miss the reference
+#      Use: qiime quality-control exclude-seqs [OPTIONS]
+
+qiime quality-control exclude-seqs --i-query-sequences RepSeq.qza \
+      					     --i-reference-sequences RepSeq.qza \
+      					     --p-method vsearch \
+      					     --p-threads 6 \
+      					     --p-perc-identity 1.00 \
+      					     --p-perc-query-aligned 1.00 \
+      					     --o-sequence-hits HitNegCtrl.qza \
+      					     --o-sequence-misses NegRepSeq.qza
+
+# table_contamination_filter :
+##############################
+
+# Aim: filter features from table based on frequency and/or metadata
+#      Use: qiime feature-table filter-features [OPTIONS]
+
+qiime feature-table filter-features --i-table Table.qza \
+     					      --m-metadata-file HitNegCtrl.qza \
+     					      --o-filtered-table NegTable.qza \
+     					      --p-exclude-ids
+
+# table_contingency_filter :
+############################
+
+# Aim: filter features that show up in only one samples, based on
+#      the suspicion that these may not represent real biological diversity
+#      but rather PCR or sequencing errors (such as PCR chimeras)
+#      Use: qiime feature-table filter-features [OPTIONS]
+
+# contingency:
+    # min_obs: 2  # Remove features that are present in only a single sample !
+    # min_freq: 0 # Remove features with a total abundance (summed across all samples) of less than 0 !
+
+
+qiime feature-table filter-features  --i-table NegTable.qza \
+        					       --p-min-samples 2 \
+        					       --p-min-frequency 0 \
+        					       --o-filtered-table ConTable.qza
+
+
+# sequence_contingency_filter :
+###############################
+
+# Aim: Filter features from sequence based on table and/or metadata
+       # Use: qiime feature-table filter-seqs [OPTIONS]
+
+qiime feature-table filter-seqs --i-data NegRepSeq.qza \
+      					  --i-table ConTable.qza \
+      					  --o-filtered-data ConRepSeq.qza
+
+
+# sequence_summarize :
+######################
+
+# Aim: Generate tabular view of feature identifier to sequence mapping
+       # Use: qiime feature-table tabulate-seqs [OPTIONS]
+
+qiime feature-table tabulate-seqs --i-data RepSeq.qza --o-visualization RepSeq.qzv
+qiime feature-table tabulate-seqs --i-data ConRepSeq.qza --o-visualization ConRepSeq.qzv
+qiime feature-table tabulate-seqs --i-data NegRepSeq.qza --o-visualization NegRepSeq.qzv
+
+# table_summarize :
+###################
+
+qiime feature-table summarize --i-table Table.qza \
+        --m-sample-metadata-file sample-metadata.tsv \
+        --o-visualization Table.qzv
+
+qiime feature-table summarize --i-table ConTable.qza \
+        --m-sample-metadata-file sample-metadata.tsv \
+        --o-visualization ConTable.qzv
+
+qiime feature-table summarize --i-table NegTable.qza \
+        --m-sample-metadata-file sample-metadata.tsv \
+        --o-visualization NegTable.qzv
+
 
 ###############################################################
 ### For Bacteria
@@ -47,6 +129,13 @@ cd $DATADIRECTORY_V4_bacteria
 eval "$(conda shell.bash hook)"
 conda activate qiime2-2019.10
 
+# dada2_denoise :
+#################
+
+# Aim: denoises single-end sequences, dereplicates them, and filters
+#      chimeras and singletons sequences
+#      Use: qiime dada2 denoise-single [OPTIONS]
+
 qiime dada2 denoise-single --i-demultiplexed-seqs demux.qza \
                            --p-n-threads 6 \
                            --p-chimera-method 'consensus' \
@@ -55,12 +144,87 @@ qiime dada2 denoise-single --i-demultiplexed-seqs demux.qza \
                            --o-representative-sequences RepSeq.qza \
                            --o-table Table.qza \
                            --o-denoising-stats Stats.qza
-                           
-                           
-qiime feature-table summarize \
-  --i-table Table.qza \
-  --o-visualization Table.qzv \
-  --m-sample-metadata-file sample-metadata.tsv
-qiime feature-table tabulate-seqs \
-  --i-data RepSeq.qza \
-  --o-visualization RepSeq.qzv    
+
+# sequence_contamination_filter :
+#################################
+
+# Aim: aligns feature sequences to a set of reference sequences
+#      to identify sequences that hit/miss the reference
+#      Use: qiime quality-control exclude-seqs [OPTIONS]
+
+qiime quality-control exclude-seqs --i-query-sequences RepSeq.qza \
+      					     --i-reference-sequences RepSeq.qza \
+      					     --p-method vsearch \
+      					     --p-threads 6 \
+      					     --p-perc-identity 1.00 \
+      					     --p-perc-query-aligned 1.00 \
+      					     --o-sequence-hits HitNegCtrl.qza \
+      					     --o-sequence-misses NegRepSeq.qza
+
+# table_contamination_filter :
+##############################
+
+# Aim: filter features from table based on frequency and/or metadata
+#      Use: qiime feature-table filter-features [OPTIONS]
+
+qiime feature-table filter-features --i-table Table.qza \
+     					      --m-metadata-file HitNegCtrl.qza \
+     					      --o-filtered-table NegTable.qza \
+     					      --p-exclude-ids
+
+# table_contingency_filter :
+############################
+
+# Aim: filter features that show up in only one samples, based on
+#      the suspicion that these may not represent real biological diversity
+#      but rather PCR or sequencing errors (such as PCR chimeras)
+#      Use: qiime feature-table filter-features [OPTIONS]
+
+# contingency:
+    # min_obs: 2  # Remove features that are present in only a single sample !
+    # min_freq: 0 # Remove features with a total abundance (summed across all samples) of less than 0 !
+
+
+qiime feature-table filter-features  --i-table NegTable.qza \
+        					       --p-min-samples 2 \
+        					       --p-min-frequency 0 \
+        					       --o-filtered-table ConTable.qza
+
+
+# sequence_contingency_filter :
+###############################
+
+# Aim: Filter features from sequence based on table and/or metadata
+       # Use: qiime feature-table filter-seqs [OPTIONS]
+
+qiime feature-table filter-seqs --i-data NegRepSeq.qza \
+      					  --i-table ConTable.qza \
+      					  --o-filtered-data ConRepSeq.qza
+
+
+# sequence_summarize :
+######################
+
+# Aim: Generate tabular view of feature identifier to sequence mapping
+       # Use: qiime feature-table tabulate-seqs [OPTIONS]
+
+qiime feature-table tabulate-seqs --i-data RepSeq.qza --o-visualization RepSeq.qzv
+qiime feature-table tabulate-seqs --i-data ConRepSeq.qza --o-visualization ConRepSeq.qzv
+qiime feature-table tabulate-seqs --i-data NegRepSeq.qza --o-visualization NegRepSeq.qzv
+
+# table_summarize :
+###################
+
+qiime feature-table summarize --i-table Table.qza \
+        --m-sample-metadata-file sample-metadata.tsv \
+        --o-visualization Table.qzv
+
+qiime feature-table summarize --i-table ConTable.qza \
+        --m-sample-metadata-file sample-metadata.tsv \
+        --o-visualization ConTable.qzv
+
+qiime feature-table summarize --i-table NegTable.qza \
+        --m-sample-metadata-file sample-metadata.tsv \
+        --o-visualization NegTable.qzv
+
+   
