c0359947e5d557fa2479c83b53dd26579abd2fdb
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2021-05-17 14:38:16 +1100

    Update zz_junk.sh

--- 00_scripts/zz_junk.sh
+++ 00_scripts/zz_junk.sh
@@ -15,8 +15,11 @@ conda activate qiime2-2019.10
 # Aim: Filter features from sequence based on table and/or metadata
        # Use: qiime feature-table filter-seqs [OPTIONS]
 
-qiime feature-table filter-seqs --i-data NegRepSeq.qza \
-      					  --i-table ConTable.qza \
-      					  --o-filtered-data ConRepSeq.qza \
-                  --output-dir /Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_IAC/02_Data/05_Mare_ignames/Diversity_in_Mare_yam_crop/05_QIIME2/ITS2/out
+#qiime feature-table filter-seqs --i-data NegRepSeq.qza \
+#      					  --i-table ConTable.qza \
+#      					  --o-filtered-data ConRepSeq.qza \
+#                  --output-dir /Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_IAC/02_Data/05_Mare_ignames/Diversity_in_Mare_yam_crop/05_QIIME2/ITS2/out
+
+qiime feature-table filter-seqs --i-data NegRepSeq.qza --i-table ConTable.qza --o-filtered-data ConRepSeq.qza
+
 
