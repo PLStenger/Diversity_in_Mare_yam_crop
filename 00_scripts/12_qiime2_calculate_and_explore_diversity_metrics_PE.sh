a708627ab10582199031fc1b10d0aaa954c85687
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2021-06-11 15:30:09 +1100

    Update 12_qiime2_calculate_and_explore_diversity_metrics_PE.sh

--- 00_scripts/12_qiime2_calculate_and_explore_diversity_metrics_PE.sh
+++ 00_scripts/12_qiime2_calculate_and_explore_diversity_metrics_PE.sh
@@ -3,6 +3,9 @@
 DATADIRECTORY_ITS2_fungi=/Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_IAC/02_Data/05_Mare_ignames/Diversity_in_Mare_yam_crop/05_QIIME2/Paired_end/ITS2/
 DATADIRECTORY_V4_bacteria=/Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_IAC/02_Data/05_Mare_ignames/Diversity_in_Mare_yam_crop/05_QIIME2/Paired_end/V4/
 
+METADATA_ITS2=/Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_IAC/02_Data/05_Mare_ignames/Diversity_in_Mare_yam_crop/98_database_files/ITS2/
+METADATA_V4=/Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_IAC/02_Data/05_Mare_ignames/Diversity_in_Mare_yam_crop/98_database_files/V4/
+
 # Aim: perform diversity metrics and rarefaction
 
 # https://chmi-sops.github.io/mydoc_qiime2.html#step-8-calculate-and-explore-diversity-metrics
@@ -30,7 +33,7 @@ qiime diversity core-metrics-phylogenetic \
   --i-phylogeny rooted-tree.qza \
   --i-table Table.qza \
   --p-sampling-depth 16708 \
-  --m-metadata-file sample-metadata.tsv \
+  --m-metadata-file $METADATA_ITS2/sample-metadata.tsv \
   --output-dir core-metrics-results
   
 # Now test for relationships between alpha diversity and study metadata   
@@ -96,7 +99,7 @@ qiime diversity core-metrics-phylogenetic \
   --i-phylogeny rooted-tree.qza \
   --i-table Table.qza \
   --p-sampling-depth 2634 \
-  --m-metadata-file sample-metadata.tsv \
+  --m-metadata-file $METADATA_V4/sample-metadata.tsv \
   --output-dir core-metrics-results
 
 # Now test for relationships between alpha diversity and study metadata   
