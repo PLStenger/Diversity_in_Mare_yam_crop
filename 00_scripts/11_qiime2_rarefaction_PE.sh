af5dcd2d23f0ea6fd3dc28c6de127d865a014218
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2021-06-11 14:41:54 +1100

    Update 11_qiime2_rarefaction_PE.sh

--- 00_scripts/11_qiime2_rarefaction_PE.sh
+++ 00_scripts/11_qiime2_rarefaction_PE.sh
@@ -22,7 +22,7 @@ conda activate qiime2-2019.10
 qiime diversity alpha-rarefaction \
   --i-table Table.qza \
   --i-phylogeny rooted-tree.qza \
-  --p-max-depth 57509 \
+  --p-max-depth 50124 \
   --m-metadata-file sample-metadata.tsv \
   --o-visualization alpha-rarefaction.qzv
   
@@ -46,7 +46,7 @@ conda activate qiime2-2019.10
 qiime diversity alpha-rarefaction \
   --i-table Table.qza \
   --i-phylogeny rooted-tree.qza \
-  --p-max-depth 88093 \
+  --p-max-depth 23698 \
   --m-metadata-file sample-metadata.tsv \
   --o-visualization alpha-rarefaction.qzv
   
