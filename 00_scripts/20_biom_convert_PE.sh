7ff78f8810763d3c68b7bdfc0e98c26b7a4cf1cd
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2021-06-21 16:58:01 +1100

    Create 20_biom_convert_PE.sh

---
+++ 00_scripts/20_biom_convert_PE.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+DATADIRECTORY_ITS2_fungi=/Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_IAC/02_Data/05_Mare_ignames/Diversity_in_Mare_yam_crop/05_QIIME2/Paired_end/ITS2/
+DATADIRECTORY_V4_bacteria=/Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_IAC/02_Data/05_Mare_ignames/Diversity_in_Mare_yam_crop/05_QIIME2/Paired_end/V4/
+
+METADATA_ITS2=/Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_IAC/02_Data/05_Mare_ignames/Diversity_in_Mare_yam_crop/98_database_files/ITS2/
+METADATA_V4=/Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_IAC/02_Data/05_Mare_ignames/Diversity_in_Mare_yam_crop/98_database_files/V4/
+
+
+###############################################################
+### For Fungi
+###############################################################
+
+cd $DATADIRECTORY_ITS2_fungi
+
+eval "$(conda shell.bash hook)"
+conda activate qiime2-2019.10
+
+# Aim: Convert to/from the BIOM table format
+
+biom convert \
+        -i {input.biom_file} \
+        -o {output.tsv_file} \
+        --to-tsv
