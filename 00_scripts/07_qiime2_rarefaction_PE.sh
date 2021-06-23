63b6838ef032dd743db7a41e4ab7c3b3a5715c9a
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2021-06-23 14:04:17 +1100

    Update and rename 11_qiime2_rarefaction_PE.sh to 07_qiime2_rarefaction_PE.sh

--- 00_scripts/07_qiime2_rarefaction_PE.sh
+++ 00_scripts/07_qiime2_rarefaction_PE.sh
@@ -23,17 +23,39 @@ conda activate qiime2-2019.10
 # Note: max-depth should be chosen based on Table.qzv
 
 qiime diversity alpha-rarefaction \
-  --i-table Table.qza \
-  --i-phylogeny rooted-tree.qza \
+  --i-table core/Table.qza \
+  --i-phylogeny tree/rooted-tree.qza \
   --p-max-depth 50124 \
+  --p-min-depth 1 \
   --m-metadata-file $METADATA_ITS2/sample-metadata.tsv \
-  --o-visualization alpha-rarefaction.qzv
+  --o-visualization visual/alpha-rarefaction.qzv
   
 # Note: Interpreting alpha diversity metrics: it is important to understand that certain metrics are stricly qualitative (presence/absence), 
 # that is they only take diversity into account, often referred to as richness of the community (e.g. observed otus). 
 # In contrast, other methods are quantitative in that they consider both richness and abundance across samples, commonly referred to as evenness (e.g. Shannon). 
 # Yet other methods take phylogenetic distance into account by asking how diverse the phylogenetic tree is for each sample. 
 # These phylogenetic tree-based methods include the popular Faith’s PD, which calculates the sum of the branch length covered by a sample
+  
+  
+#clustering method:
+        #- 'nj'   # neighbor joining
+        #- 'upgma' # UPGMA, an arbitrary rarefaction trial will be used for the tree, and the remaining trials
+                   # are used to calculate the support of the internal nodes of that tree.
+                   
+# --p-sampling-depth # The total frequency that each sample should be rarefied to prior to computing the diversity metric.  
+# --p-sampling-depth # Besoin de se baser sur les resultats de la rarefaction
+
+qiime diversity beta-rarefaction \
+        --i-table core/ConTable.qza \
+        --i-phylogeny tree/rooted-tree.qza \
+        --m-metadata-file $METADATA_ITS2/sample-metadata.tsv \ 
+        --p-clustering-method upgma \ 
+        --p-sampling-depth 16708 \ 
+        --p-iterations 10 \
+        --p-correlation-method spearman \
+        --p-color-scheme BrBG \
+        --o-visualization visual/RareGraph-beta.qzv
+  
   
 ###############################################################
 ### For Bacteria
@@ -47,14 +69,34 @@ conda activate qiime2-2019.10
 # Note: max-depth should be chosen based on Yable.qzv
 
 qiime diversity alpha-rarefaction \
-  --i-table Table.qza \
-  --i-phylogeny rooted-tree.qza \
+  --i-table core/Table.qza \
+  --i-phylogeny tree/rooted-tree.qza \
   --p-max-depth 23698 \
+  --p-min-depth 1 \
   --m-metadata-file $METADATA_V4/sample-metadata.tsv \
-  --o-visualization alpha-rarefaction.qzv
+  --o-visualization visual/alpha-rarefaction.qzv
   
 # Note: Interpreting alpha diversity metrics: it is important to understand that certain metrics are stricly qualitative (presence/absence), 
 # that is they only take diversity into account, often referred to as richness of the community (e.g. observed otus). 
 # In contrast, other methods are quantitative in that they consider both richness and abundance across samples, commonly referred to as evenness (e.g. Shannon). 
 # Yet other methods take phylogenetic distance into account by asking how diverse the phylogenetic tree is for each sample. 
 # These phylogenetic tree-based methods include the popular Faith’s PD, which calculates the sum of the branch length covered by a sample
+
+#clustering method:
+        #- 'nj'   # neighbor joining
+        #- 'upgma' # UPGMA, an arbitrary rarefaction trial will be used for the tree, and the remaining trials
+                   # are used to calculate the support of the internal nodes of that tree.
+                   
+# --p-sampling-depth # The total frequency that each sample should be rarefied to prior to computing the diversity metric.  
+# --p-sampling-depth # Besoin de se baser sur les resultats de la rarefaction
+
+qiime diversity beta-rarefaction \
+        --i-table core/ConTable.qza \
+        --i-phylogeny tree/rooted-tree.qza \
+        --m-metadata-file $METADATA_V4/sample-metadata.tsv \ 
+        --p-clustering-method upgma \ 
+        --p-sampling-depth 2634 \ 
+        --p-iterations 10 \
+        --p-correlation-method spearman \
+        --p-color-scheme BrBG \
+        --o-visualization visual/RareGraph-beta.qzv
