64575b5ffc4a92eb9a5e1318fde6a4773b9d31e5
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2021-06-23 11:18:56 +1100

    Update 03_cleaned_data_quality_check_by_FastQC.sh

--- 00_scripts/03_cleaned_data_quality_check_by_FastQC.sh
+++ 00_scripts/03_cleaned_data_quality_check_by_FastQC.sh
@@ -11,16 +11,26 @@ DATA_DIRECTORY_V4_bacteria=/Users/pierre-louisstenger/Documents/PostDoc_02_MetaB
 OUT_PUT_ITS2_fungi=/Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_IAC/02_Data/05_Mare_ignames/Diversity_in_Mare_yam_crop/03_cleaned_data_quality_check/OUT_PUT_ITS2_fungi/
 OUT_PUT_V4_bacteria=/Users/pierre-louisstenger/Documents/PostDoc_02_MetaBarcoding_IAC/02_Data/05_Mare_ignames/Diversity_in_Mare_yam_crop/03_cleaned_data_quality_check/OUT_PUT_V4_bacteria/
 
-cd $WORKING_DIRECTORY
+cd $DATA_DIRECTORY_ITS2_fungi
 
-## For ITS2_fungi
-#for FILE in $(ls $DATA_DIRECTORY_ITS2_fungi/*.fastq.gz)
-#do
-#      fastqc $FILE -o $OUT_PUT_ITS2_fungi
-#done ;
+# For ITS2_fungi
+for FILE in $(ls $DATA_DIRECTORY_ITS2_fungi/*.fastq.gz)
+do
+      fastqc $FILE -o $OUT_PUT_ITS2_fungi
+done ;
+
+# Run multiqc for quality summary
+
+multiqc .
+
+cd $DATA_DIRECTORY_V4_bacteria
 
 # For V4_bacteria
 for FILE in $(ls $DATA_DIRECTORY_V4_bacteria/*.fastq.gz)
 do
       fastqc $FILE -o $OUT_PUT_V4_bacteria
 done ;
+
+# Run multiqc for quality summary
+
+multiqc .
