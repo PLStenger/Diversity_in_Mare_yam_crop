4132cb8453de12137b92f0ac2ce6c8d0589eba65
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2021-06-28 11:11:38 +1100

    Update zz_junk.sh

--- 00_scripts/zz_junk.sh
+++ 00_scripts/zz_junk.sh
@@ -17,12 +17,30 @@ cd $DATADIRECTORY_ITS2_fungi
 eval "$(conda shell.bash hook)"
 conda activate qiime2-2019.10
 
+qiime feature-table filter-samples \
+        --i-table core/RarTable.qza \
+        --m-metadata-file $METADATA_ITS2/sample-metadata.tsv \
+        --p-where "[#SampleID] IN ('F1', 'F2', 'F3', 'F4', 'F5', 'SF1', 'SF2', 'SF3', 'SF4', 'SF5', 'LF1', 'LF2', 'LF3', 'LF4', 'LF5')"  \
+        --o-filtered-table subtables/RarTable-all.qza
+        
+qiime feature-table core-features \
+        --i-table subtables/RarTable-all.qza \
+        --p-min-fraction 0.1 \
+        --p-max-fraction 1.0 \
+        --p-steps 10 \
+        --o-visualization visual/CoreBiom-all.qzv    
+        
+  # CoreBiomAll.qzv  a suprrimer ?      
+        
+        
+
 qiime tools export --input-path subtables/RarTable-Forest.qza --output-path export/subtables/RarTable-Forest 
 qiime tools export --input-path subtables/RarTable-Long_fallow.qza --output-path export/subtables/RarTable-Long_fallow 
 qiime tools export --input-path subtables/RarTable-short_fallow.qza --output-path export/subtables/RarTable-short_fallow       
-qiime tools export --input-path core/RarTable.qza --output-path export/subtables/RarTable_all    
+qiime tools export --input-path subtables/RarTable-all.qza --output-path export/subtables/RarTable-all    
         
-qiime tools export --input-path visual/CoreBiomAll.qzv --output-path export/visual/CoreBiomAll
+#qiime tools export --input-path visual/CoreBiomAll.qzv --output-path export/visual/CoreBiomAll
+qiime tools export --input-path visual/CoreBiom-all.qzv --output-path export/visual/CoreBiom-all.qzv
 qiime tools export --input-path visual/CoreBiom-Long_fallow.qzv --output-path export/visual/CoreBiom-Long_fallow
 qiime tools export --input-path visual/CoreBiom-short_fallow.qzv --output-path export/visual/CoreBiom-short_fallow
 qiime tools export --input-path visual/CoreBiom-Forest.qzv --output-path export/visual/CoreBiom-Forest
