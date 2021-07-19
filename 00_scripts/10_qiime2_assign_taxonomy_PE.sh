ebbef6572770b6cf7fc2dd107d4a46b224b60c0e
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2021-07-20 10:02:43 +1100

    Update 10_qiime2_assign_taxonomy_PE.sh

--- 00_scripts/10_qiime2_assign_taxonomy_PE.sh
+++ 00_scripts/10_qiime2_assign_taxonomy_PE.sh
@@ -242,9 +242,9 @@ echo '##########################################################################
         
 ## You will need to rename the files like that : 
 
-mv SILVA-v138-515f-806r-consensus-classifier.qza Classifier.qza
-mv SILVA-138-SSURef-515f-806r-Seqs.qza DataSeq.qza
-mv SILVA-v138-515f-806r-consensus-taxonomy.qza RefTaxo.qza
+mv taxonomy/SILVA-v138-515f-806r-consensus-classifier.qza taxonomy/Classifier.qza
+mv taxonomy/SILVA-138-SSURef-515f-806r-Seqs.qza taxonomy/DataSeq.qza
+mv taxonomy/SILVA-v138-515f-806r-consensus-taxonomy.qza taxonomy/RefTaxo.qza
 
 # Aim: Create a scikit-learn naive_bayes classifier for reads
 
